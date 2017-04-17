323c324d12bcaca27c64135e38f4659f3e6d7f3a
Jacksgong <jacks.gong@alipay.com>
2017-04-18 02:03:18 +0800

    feat: implement the logic to crash king of glory game

--- app/src/main/java/cn/dreamtobe/xposed/crashpvp/CrashPVP.java
+++ app/src/main/java/cn/dreamtobe/xposed/crashpvp/CrashPVP.java
@@ -1,7 +1,12 @@
 package cn.dreamtobe.xposed.crashpvp;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
+import static de.robv.android.xposed.XposedHelpers.findClass;
 
 /**
  * Created by Jacksgong on 18/04/2017.
@@ -12,5 +17,19 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class CrashPVP implements IXposedHookLoadPackage {
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+        if (!lpparam.packageName.equals("com.tencent.tmgp.sgame")) return;
+
+        if (BuildConfig.DEBUG) {
+            XposedBridge.log("dreamtobe in king of glory!");
+        }
+
+        final Class<?> crashNotifyHandler = findClass("com.tsf4g.apollo.report.CrashNotifyHandler", lpparam.classLoader);
+        findAndHookMethod(crashNotifyHandler, "Instance", new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                super.afterHookedMethod(param);
+                param.setResult(null);
+            }
+        });
     }
 }
